47e2228797cb5d8ce60a91552730641f4160328c
Mehdi Rahmani <mahdi0131rain@gmail.com>
2023-11-21 22:02:24 +0330

    refactor: Improve clarity and conciseness in ReactTransition component
    - Refactored the function name for better clarity
    - Simplified the logic for constructing class names
    - Cleaned up ref handling for improved readability
    - Removed redundant code related to checking value and updating isShow
    - Made variable and function names more consistent

--- src/components/ReactTransition.tsx
+++ src/components/ReactTransition.tsx
@@ -1,67 +1,11 @@
-import {createElement, forwardRef, ReactElement, useEffect, useRef, useState} from 'react'
-
-export const ReactTransition =
-    forwardRef(function AppTransition(
-            {
-                children,
-                value = true,
-                name = 'fade',
-                onTransitionend
-            }: TransitionProps, ref) {
-
-            const [isShow, setIsShow] = useState(value)
-            const localeTransitionRef = useRef<HTMLDivElement | null>(null)
-            const timeoutRef = useRef<number>(0)
-
-            let transitionRef: HTMLDivElement
-
-            if (!isShow && value) {
-                setIsShow(value)
-            }
-
-            const childrenClasses = children.props.className
-                ? `${children.props.className} `
-                : ''
-            const className = `${childrenClasses}${name}-enter-from ${name}-enter-active`
-
-            const transitionElement = createElement(children.type, {
-                ...children.props,
-                className,
-                ref: (node: HTMLDivElement) => {
-                    transitionRef = localeTransitionRef.current = node;
-                    if (typeof ref === 'function') {
-                        ref(node);
-                    } else if (ref) {
-                        transitionRef = ref.current = node;
-                    }
-                },
-                onTransitionEnd: () => {
-                    if (!value) {
-                        setIsShow(false)
-                    }
-                    if (onTransitionend) {
-                        onTransitionend()
-                    }
-                },
-            })
-
-            useEffect(() => {
-                if (transitionRef === null) return
-                timeoutRef.current = setTimeout(() => {
-                    if (value && transitionRef) {
-                        transitionRef.classList.add(`${name}-enter-to`)
-                        transitionRef.classList.remove(`${name}-enter-from`)
-                    } else if (transitionRef) {
-                        transitionRef.classList.remove(`${name}-enter-to`)
-                        transitionRef.classList.add(`${name}-enter-from`)
-                    }
-                }, 0)
-                return () => clearTimeout(timeoutRef.current)
-            }, [value])
-
-            return isShow ? transitionElement : ''
-        }
-    )
+import {
+    createElement,
+    ReactElement,
+    forwardRef,
+    useEffect,
+    useState,
+    useRef
+} from 'react'
 
 interface TransitionProps {
     children: ReactElement,
@@ -69,3 +13,61 @@ interface TransitionProps {
     name?: string,
     onTransitionend?: () => boolean,
 }
+
+export const ReactTransition = forwardRef(
+    function ReactTransition({children, value = true, name = 'fade', onTransitionend}: TransitionProps
+        , externalRef
+    ) {
+
+        const [isShow, setIsShow] = useState(value)
+        const localeTransitionRef = useRef<HTMLDivElement | null>(null)
+        const timeoutRef = useRef<number>(0)
+
+        const setRef = (node: HTMLDivElement) => {
+            localeTransitionRef.current = node;
+            if (typeof externalRef === 'function') {
+                externalRef(node);
+            } else if (externalRef) {
+                externalRef.current = node;
+            }
+        }
+
+        if (!isShow && value) {
+            setIsShow(value)
+        }
+
+        const childrenClasses = children.props.className ? `${children.props.className} ` : ''
+        const className = `${childrenClasses}${name}-enter-from ${name}-enter-active`
+
+        const transitionElement = createElement(children.type, {
+            ...children.props,
+            className,
+            ref: setRef,
+            onTransitionEnd: () => {
+                if (!value) {
+                    setIsShow(false)
+                }
+                if (onTransitionend) {
+                    onTransitionend()
+                }
+            },
+        })
+
+        useEffect(() => {
+            const { current: transitionRef } = localeTransitionRef;
+            if (!transitionRef) return;
+
+            timeoutRef.current = setTimeout(() => {
+                if (transitionRef) {
+                    const actionClass = value ? 'remove' : 'add';
+                    transitionRef.classList[actionClass](`${name}-enter-to`);
+                    transitionRef.classList[actionClass](`${name}-enter-from`);
+                }
+            }, 0);
+
+            return () => clearTimeout(timeoutRef.current)
+        }, [value, name])
+
+        return isShow ? transitionElement : ''
+    }
+)
